9d6a2c455fb95426d0e4b44862a4c6d98a960f9d
JoshMusira <joshuamusira01@gmail.com>
2023-08-14 23:56:54 +0300

    feat: Create Auth UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import { StreamChat } from 'stream-chat'
 import { Chat } from 'stream-chat-react'
 import Cookies from 'universal-cookie'
 
-import { ChannelContainer, ChannelListContainer } from './exports/exports'
+import { Auth, ChannelContainer, ChannelListContainer } from './exports/exports'
 
 const apikey = 'sjfxze9b8c24'
 
@@ -12,6 +12,9 @@ const apikey = 'sjfxze9b8c24'
 const client = StreamChat.getInstance(apikey);
 
 function App() {
+  let authToken = false
+
+  if (!authToken) return <Auth />
 
   return (
     <div className="app__wrapper">
--- client/src/exports/exports.js
+++ client/src/exports/exports.js
@@ -1,3 +1,4 @@
+
 export { default as ChannelContainer } from '../components/channelContainer/ChannelContainer';
 export { default as ChannelListContainer } from '../components/channelListContainer/ChannelListContainer';
 export { default as SideBar } from '../components/sidebar/SideBar'
@@ -5,3 +6,4 @@ export { default as CompanyHeader } from '../components/companyHeader/CompanyHea
 export { default as ChannelSearch } from '../components/channelSearch/ChannelSearch'
 export { default as TeamChannelList } from '../components/channelList/TeamChannelList'
 export { default as TeamChannelPreview } from '../components/teamchannelpreview/TeamChannelPreview'
+export { default as Auth } from '../pages/Auth'
